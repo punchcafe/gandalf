11d7460d2e1d6dd89ba948cfc92c8b92097b63e2
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-06-19 14:45:24 -0500

    :sparkles: add simple chatGPT data_structure questions

--- lib/gandalf/question/repo.ex
+++ lib/gandalf/question/repo.ex
@@ -3,14 +3,25 @@ defmodule Gandalf.Question.Repo do
     alias Gandalf.Question
     
     def all() do
-        "./resources/questions/simple.yml"
-        |> YamlElixir.read_from_file!()
-        |> Map.get("questions")
-        |> Enum.map(&yml_to_model/1)
+        all("./resources/questions/")
     end
 
     defp yml_to_model(yml_model) do
         %{"text" => text, "answers" => answers, "correct_answer" => correct_answer} = yml_model
         %Question{question_body: text, answer_choices: answers, correct_answer_index: correct_answer - 1}
+    end
+
+    defp all(file) do
+        if File.dir?(file) do
+            file
+            |> File.ls!()
+            |> Enum.map(& file <> &1)
+            |> Enum.flat_map(&all/1)
+        else
+            file
+            |> YamlElixir.read_from_file!()
+            |> Map.get("questions")
+            |> Enum.map(&yml_to_model/1)
+        end
     end
 end
