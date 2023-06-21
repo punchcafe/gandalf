971a08aac3270ff7f68ec0372881e8c702fa6a36
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-06-21 18:12:32 -0500

    :sparkles: implement Insight module

--- lib/gandalf/session/insight.ex
+++ lib/gandalf/session/insight.ex
@@ -1,7 +1,24 @@
 defmodule Gandalf.Session.Insight do
-  alias Gandalf.Session.Result
+  alias Gandalf.Topic
+  alias Gandalf.Session
 
-  # @type suggestions :: map()
+  def last_depth_insight(%Session{questions: questions, answers: answers}) do
+    last_depth = questions |> List.last() |> Map.get(:topic) |> Topic.depth()
 
-  # @spec suggest_topics(Result.t()) :: suggestions()
+    questions
+    |> Stream.zip(answers)
+    # only analyze last depth
+    |> Stream.filter(fn {%{topic: topic}, _} -> Topic.depth(topic) == last_depth end)
+    |> Stream.map(fn {%{topic: topic, correct_answer_index: correct_answer}, user_answer} ->
+      {topic, correct_answer == user_answer}
+    end)
+    |> Enum.group_by(fn {topic, _?} -> topic end, fn {_, correct?} -> correct? end)
+    |> Enum.map(fn {topic, answers} -> {topic, average_correct_answers(answers)} end)
+    |> Enum.into(%{})
+  end
+
+  defp average_correct_answers(answers) do
+    total_correct = answers |> Enum.filter(& &1) |> Enum.count()
+    total_correct / Enum.count(answers)
+  end
 end
