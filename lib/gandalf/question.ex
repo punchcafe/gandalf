cda4383ee2faaea6f374237d3d30b47d7ee9a42a
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-06-21 16:15:16 -0500

    :sparkles: add topic field to questions

--- lib/gandalf/question.ex
+++ lib/gandalf/question.ex
@@ -6,9 +6,10 @@ defmodule Gandalf.Question do
         }
 
   @type t :: %__MODULE__{
+          topic: Topic.t(),
           question_body: String.t(),
           answer_choices: [String.t()],
           correct_answer_index: integer()
         }
-  defstruct [:question_body, :answer_choices, :correct_answer_index]
+  defstruct [:question_body, :answer_choices, :correct_answer_index, :topic]
 end
--- lib/gandalf/question/repo.ex
+++ lib/gandalf/question/repo.ex
@@ -8,8 +8,7 @@ defmodule Gandalf.Question.Repo do
     |> read_all()
     |> Enum.filter(&matches_depth?(&1, depth))
     |> Enum.reject(fn %{"topic" => topic} -> topic_excluded?(topic, exclude_list) end)
-    |> Enum.flat_map(&Map.get(&1, "questions"))
-    |> Enum.map(&yml_to_model/1)
+    |> Enum.map(&extract_questions/1)
   end
 
   defp topic_excluded?(topic, exclude_list) do
@@ -18,15 +17,20 @@ defmodule Gandalf.Question.Repo do
     end)
   end
 
+  defp extract_questions(yaml_document = %{"topic" => topic, "questions" => questions}) do
+    Enum.map(questions, &yml_to_model(&1, topic))
+  end
+
   defp matches_depth?(_resouce_yaml = %{"topic" => topic}, depth), do: Topic.depth(topic) == depth
 
-  defp yml_to_model(yml_model) do
+  defp yml_to_model(yml_model, topic) do
     %{"text" => text, "answers" => answers, "correct_answer" => correct_answer} = yml_model
 
     %Question{
       question_body: text,
       answer_choices: answers,
-      correct_answer_index: correct_answer - 1
+      correct_answer_index: correct_answer - 1,
+      topic: topic
     }
   end
 
