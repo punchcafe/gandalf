6735d9fbc11f8e3d92dc154c7fb9c7714d2e37e4
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-06-23 16:46:37 -0500

    :sparkles::recycle: refactor Session.Insight and Session implementation for more composability

--- lib/gandalf/session.ex
+++ lib/gandalf/session.ex
@@ -3,34 +3,23 @@ defmodule Gandalf.Session do
   alias Gandalf.Topic.Repo, as: TopicRepo
   alias Gandalf.Topic
   alias __MODULE__.Insight
-  # @type session_configuration :: map()
-  # @type session :: String.t()
-  # @type question_id :: String.t()
 
-  # @spec new_session(session_configuration()) :: {:ok, session()} | {:error, atom()}
-  # @spec new_session!() :: session() | no_return()
-
-  # @spec current_question(session) :: {:ok, question_id()} | :finished | {:error, atom()}
-
-  # @spec submit_answer(session(), answer_index :: integer()) :: :ok | {:error, atom()}
+  @finish_failed_topic_count 5
 
   @type question :: %{
           question_body: String.t(),
           answer_choices: [String.t()],
           correct_answer_index: integer()
         }
-  defstruct [:answers, :questions, :failed_topics]
+  defstruct [:answers, :questions]
 
-  def new(), do: %__MODULE__{answers: [], questions: Repo.all(1), failed_topics: []}
+  def new(), do: %__MODULE__{answers: [], questions: Repo.all(1)}
 
   def next_question(%__MODULE__{
         answers: answers,
         questions: questions
       }) do
     next_answer_index = Enum.count(answers)
-
-    # TODO, make raise?
-    # Maybe make this function get current_question, so that it's always tied to a submit answer
 
     if next_answer_index < Enum.count(questions) do
       {:ok, Enum.at(questions, Enum.count(answers))}
@@ -43,10 +32,15 @@ defmodule Gandalf.Session do
     if Enum.count(answers) < Enum.count(session.questions) do
       updated_session = %__MODULE__{session | answers: answers ++ [answer_index]}
 
-      if Enum.count(updated_session.answers) == Enum.count(session.questions) do
-        load_next_or_finish(updated_session)
-      else
-        {:ok, updated_session}
+      cond do
+        updated_session |> Insight.failed_topics() |> Enum.count() >= @finish_failed_topic_count ->
+          {:finished, updated_session}
+
+        Enum.count(updated_session.answers) == Enum.count(session.questions) ->
+          load_next_or_finish(updated_session)
+
+        :continue ->
+          {:ok, updated_session}
       end
     else
       {:finished, session}
@@ -56,30 +50,22 @@ defmodule Gandalf.Session do
   defp load_next_or_finish(
          session = %__MODULE__{
            questions: questions,
-           answers: answers,
-           failed_topics: failed_topics_acc
+           answers: answers
          }
        ) do
     last_depth = Insight.last_question_depth(session)
 
-    {success_topics, failed_topics} =
+    success_topics =
       session
-      |> Insight.last_depth_insight()
-      |> Insight.partition_insight()
+      |> Insight.successful_topics()
+      |> Enum.filter(& Topic.depth(&1) == last_depth)
 
-    session = %__MODULE__{session | failed_topics: failed_topics ++ failed_topics_acc}
-    topics_to_include = Enum.flat_map(success_topics, &TopicRepo.subtopics/1)
-
-    with [_ | _] <- topics_to_include,
-         next_questions = [_ | _] <- Repo.all(last_depth + 1, include: topics_to_include) do
+    with [_ | _] <- success_topics,
+         next_questions = [_ | _] <- Repo.all(last_depth + 1, include: success_topics) do
       {:ok, %__MODULE__{session | questions: questions ++ next_questions}}
     else
       _ -> {:finished, session}
     end
-  end
-
-  defp load_questions(session, depth, excluding \\ []) do
-    Repo.all(depth, excliuding: excluding)
   end
 
   def submit_answer!(struct, answer_index) do
@@ -87,14 +73,7 @@ defmodule Gandalf.Session do
     struct
   end
 
-  def conclude(%__MODULE__{failed_topics: failed_topics}) do
-    "You need to practice #{Enum.join(failed_topics, " ")}."
-  end
-
-  defp get_score([], [], total), do: total
-
-  defp get_score([%{correct_answer_index: index} | questions], [answer | answers], total) do
-    total = total + if index == answer, do: 1, else: 0
-    get_score(questions, answers, total)
+  def conclude(session) do
+    "You need to practice #{session |> Insight.failed_topics() |> Enum.join(" ")}."
   end
 end
--- lib/gandalf/session/insight.ex
+++ lib/gandalf/session/insight.ex
@@ -1,45 +1,69 @@
 defmodule Gandalf.Session.Insight do
   @moduledoc ~S"""
-  Module for deriving information and state from a session struct.
+  Module for deriving insights into the current quiz session.
   """
+
   alias Gandalf.Topic
   alias Gandalf.Session
 
   @type insight :: %{Topic.t() => Float.t()}
 
   @failed_threshold 0.5
+  @questions_per_topic 5
 
-  def last_question_depth(%Session{questions: questions, answers: answers}) do
-    questions |> List.last() |> Map.get(:topic) |> Topic.depth()
+  @doc ~S"""
+  Returns the topic depth of the last question loaded in the session.
+  This won't necessarily be the last question answered by the user.
+  """
+  @spec last_question_depth(Session.t()) :: Integer.t()
+  def last_question_depth(%Session{questions: questions}) do
+    questions 
+    |> List.last() 
+    |> Map.get(:topic) 
+    |> Topic.depth()
   end
 
-  def last_depth_insight(session = %Session{questions: questions, answers: answers}) do
-    last_depth = last_question_depth(session)
+  @doc ~S"""
+  Returns a list of topics the user has failed. It will not return topics where the 
+  user hasn't completed enough questions in that topic, even if the success ratio is low.
+  """
+  @spec failed_topics(Session.t()) :: [String.t()]
+  def failed_topics(session) do
+    session
+    |> topic_success_ratios()
+    |> Stream.filter(&failed_topic?/1)
+    |> Enum.map(&extract_topic/1)
+  end
 
+  @doc ~S"""
+  Returns a list of topics the user has been successful. It will not return topics where the 
+  user hasn't completed enough questions in that topic, even if the success ratio is high.
+  """
+  @spec failed_topics(Session.t()) :: [String.t()]
+  def successful_topics(session) do
+    session
+    |> topic_success_ratios()
+    |> Stream.reject(&failed_topic?/1)
+    |> Enum.map(&extract_topic/1)
+  end
+
+  defp topic_success_ratios(session = %Session{questions: questions, answers: answers}) do
     questions
     |> Stream.zip(answers)
-    # only analyze last depth
-    |> Stream.filter(fn {%{topic: topic}, _} -> Topic.depth(topic) == last_depth end)
     |> Stream.map(fn {%{topic: topic, correct_answer_index: correct_answer}, user_answer} ->
       {topic, correct_answer == user_answer}
     end)
-    |> Enum.group_by(fn {topic, _?} -> topic end, fn {_, correct?} -> correct? end)
-    |> Enum.map(fn {topic, answers} -> {topic, average_correct_answers(answers)} end)
+    |> Enum.group_by(fn {topic, _} -> topic end, fn {_, correct?} -> correct? end)
+    |> Stream.reject(fn {_, answers} -> Enum.count(answers) < @questions_per_topic end)
+    |> Stream.map(fn {topic, answers} -> {topic, average_correct_answers(answers)} end)
     |> Enum.into(%{})
-  end
-
-  def partition_insight(insight) do
-    Enum.reduce(insight, {[], []}, fn {topic, average}, {succeded_topics, failed_topics} ->
-      if average < @failed_threshold do
-        {succeded_topics, [topic | failed_topics]}
-      else
-        {[topic | succeded_topics], failed_topics}
-      end
-    end)
   end
 
   defp average_correct_answers(answers) do
     total_correct = answers |> Enum.filter(& &1) |> Enum.count()
     total_correct / Enum.count(answers)
   end
+
+  defp failed_topic?({_topic, success_ratio}), do: success_ratio < @failed_threshold
+  defp extract_topic({topic, _}), do: topic
 end
