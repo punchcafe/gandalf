4d68bac575d04ee831cdae9445c099d7998cea2a
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-07-03 21:07:25 +0100

    :white_check_mark: add more feature tests

--- test/features/quiz_test.exs
+++ test/features/quiz_test.exs
@@ -4,6 +4,7 @@ defmodule MyApp.Features.QuizTest do
   alias Gandalf.Session
 
   @questions_per_topic 3
+  @top_level_topics 3
 
   setup do
     config =
@@ -16,16 +17,55 @@ defmodule MyApp.Features.QuizTest do
     %{session: Session.new(config)}
   end
 
-  test "Test max_topic_suggestions", %{session: session} do
+  test "Returns top level suggestion", %{session: session} do
     result = {_, session} = answer_incorrectly(session, @questions_per_topic)
     assert_test_over(result)
-    assert_suggested_topics(session, "networks")
+    assert_suggested_topics(session, "data_structures")
+  end
+
+  test "Doesn't return topic if passed, and skips sub topics if other top level topic failed", %{
+    session: session
+  } do
+    {:ok, session} = answer_correctly(session, @questions_per_topic)
+    result = {_, session} = answer_incorrectly(session, @questions_per_topic)
+
+    assert_test_over(result)
+    assert_suggested_topics(session, "databases")
+  end
+
+  test "Returns a subtopic suggestion", %{session: session} do
+    {:ok, session} = answer_correctly(session, @questions_per_topic * @top_level_topics)
+    result = {_, session} = answer_incorrectly(session, @questions_per_topic)
+
+    assert_test_over(result)
+    assert_suggested_topics(session, "databases:rds")
+  end
+
+  test "returns a mix of top level and sub level suggestions", %{session: session} do
+    session = %{session | config: %{session.config | max_topic_suggestions: 2}}
+
+    result = {_, session} = answer_incorrectly(session, @questions_per_topic)
+
+    result =
+      {_, session} = answer_correctly(session, @questions_per_topic * (@top_level_topics - 1))
+
+    result = {_, session} = answer_incorrectly(session, @questions_per_topic)
+
+    assert_test_over(result)
+    assert_suggested_topics(session, ["data_structures", "databases:rds"])
   end
 
   defp answer_incorrectly(session, number_of_times \\ 1) do
     session =
       Enum.reduce(1..number_of_times, {:ok, session}, fn _, {_, session} ->
         Session.submit_answer(session, next_incorrect_answer(session))
+      end)
+  end
+
+  defp answer_correctly(session, number_of_times \\ 1) do
+    session =
+      Enum.reduce(1..number_of_times, {:ok, session}, fn _, {_, session} ->
+        Session.submit_answer(session, next_correct_answer(session))
       end)
   end
 
@@ -47,7 +87,7 @@ defmodule MyApp.Features.QuizTest do
   end
 
   def assert_suggested_topics(session, topics) when is_list(topics) do
-    topics = Enum.join(topics, ", ")
+    topics = Enum.join(topics, " ")
     assert Session.conclude(session) == "You need to practice #{topics}."
   end
 
