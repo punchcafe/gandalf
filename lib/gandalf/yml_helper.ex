2fde3f5fe1e3f9c1d3ef02f1759bdd496bc47b01
Luca Fruzza <lfruzza@hotmail.co.uk>
2024-02-07 22:00:19 +0000

    :recycle: move resources into priv lib and reference directory by app dir

--- lib/gandalf/yml_helper.ex
+++ lib/gandalf/yml_helper.ex
@@ -1,12 +1,14 @@
 defmodule Gandalf.YmlHelper do
   def read_all(file_or_directory) do
-    if File.dir?(file_or_directory) do
-      file_or_directory
+    base_path = :gandalf |> :code.priv_dir() |> to_string()
+    full_path = base_path <> "/" <> file_or_directory
+    if File.dir?(full_path) do
+      full_path
       |> File.ls!()
       |> Enum.map(&(file_or_directory <> "/" <> &1))
       |> Enum.flat_map(&read_all/1)
     else
-      [YamlElixir.read_from_file!(file_or_directory)]
+      [YamlElixir.read_from_file!(full_path)]
     end
   end
 end
